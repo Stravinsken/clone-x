4d32670df38f1f7937791946a1cbddae3e563611
Stravinsken <Jsc78tt33@gmail.com>
2025-08-08 23:59:16 +0900

    test: feeds 동작 확인

--- backend/src/feeds/feeds.module.ts
+++ backend/src/feeds/feeds.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CoModule } from './co/co.module';
-import { SerModule } from './ser/ser.module';
 import { FeedsController } from './feeds.controller';
 import { FeedsService } from './feeds.service';
 
 @Module({
-  imports: [CoModule, SerModule],
   controllers: [FeedsController],
   providers: [FeedsService]
 })
