7069723a51f43495a9ae60f9af3c52495a4a273e
Stravinsken <Jsc78tt33@gmail.com>
2025-08-11 17:59:33 +0900

    Feat: 보안 유출 방지를 위한 .env도입

---
+++ backend/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { FeedsModule } from './feeds/feeds.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config'; 
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(), // .env 파일 읽기
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    UsersModule,
+    FeedsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
